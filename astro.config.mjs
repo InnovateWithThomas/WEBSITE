85da0eb6ddd9ccde41fe3448f4871b4e19a23e67
thomas.pazze@gmail.com <thomas.pazze@etudiant.univ-reims.fr>
2025-05-22 20:41:46 +0200

    fix: solved cloudflare adapter settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
-
+import cloudflare from "@astrojs/cloudflare";
 import preact from "@astrojs/preact";
 
 export default defineConfig({
   output: "server",
   integrations: [preact()],
+  adapter: cloudflare({}),
+  vite: {
+    ssr: {
+      external: ["node:buffer"],
+    },
+  },
 });
