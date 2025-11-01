4080ff9a55b57be8c161c96d8a41ac6631220c8f
Thomas PAZZÉ <thomas.pazze@gmail.com>
2025-11-01 03:47:58 +0100

    feat: add @astrojs/node dependency and refactor navbar burger toggle script
    - Added @astrojs/node dependency to package.json.
    - Replaced external burger.js script with inline script in BaseLayout.astro for better performance and simplicity.
    - Removed the now redundant burger.js file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
+import node from "@astrojs/node";
+
 export default defineConfig({
   output: "server",
   integrations: [preact()],
   site: 'https://innovatewiththomas.github.io',
-	base: '/',
+  base: '/',
   vite: {
     ssr: {
       external: ["node:buffer"],
     },
   },
+  adapter: node({
+    mode: "standalone",
+  }),
 });
